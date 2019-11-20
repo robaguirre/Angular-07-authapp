babf804f59f1881ecf7e3f19d0241ff83caca259
Roberto Agirre <r.agirre@ibermatica.com>
2019-11-20 15:29:17 +0100

    fin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthGuard } from './services/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'precios', component: PreciosComponent},
+  {path: 'protegida',
+    component: ProtegidaComponent,
+    canActivate: [AuthGuard]
+  },
+  {path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
